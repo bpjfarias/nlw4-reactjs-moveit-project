26f3a7b6dca0d0f55c3812bb946e30d0dd8ca95a
bpjfarias <bpjfarias@gmail.com>
2021-03-02 00:52:40 -0300

    add slash

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ export default class MyDocument extends Document {
                     <meta property="og:url" content="https://moveit-ruddy-mu.vercel.app/" />
                     <meta property="og:title" content="Home | move.it" />
                     <meta property="og:description" content="The Pomodoro technique plus gamification" />
-                    <meta property="og:image" content="images/open_graph_body.png" />
+                    <meta property="og:image" content="/images/open_graph_body.png" />
                     <link rel="shortcut icon" href="favicon.png" type="image/png" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
