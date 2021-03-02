7c67237f27aedbf61d95c58a363dda442e07d6c2
bpjfarias <bpjfarias@gmail.com>
2021-03-02 00:08:27 -0300

    updating meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,28 +1,16 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document'
 
 export default class MyDocument extends Document {
-
-     openGraphTags = {
-        currentURL: 'https://moveit-ruddy-mu.vercel.app/',
-        previewImage: 'images/open_granph_body.png',
-        siteName: 'move.it',
-        pageTitle: "Home",
-        description: 'The Pomodoro technique plus gamification',
-    }
-
     render() {
         return (
             <Html>
                 <Head>
-                    
-                    <meta property="og:url" content={this.openGraphTags.currentURL} key="ogurl" />
-                    <meta property="og:image" content={this.openGraphTags.previewImage} key="ogimage" />
-                    <meta property="og:site_name" content={this.openGraphTags.siteName} key="ogsitename" />
-                    <meta property="og:title" content={this.openGraphTags.pageTitle} key="ogtitle" />
-                    <meta property="og:description" content={this.openGraphTags.description} key="ogdesc" />   
-
-
-                    <link rel="shortcut icon" href="favicon.png" type="image/png"/>
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://moveit-ruddy-mu.vercel.app/" />
+                    <meta property="og:title" content="Home | move.it" />
+                    <meta property="og:description" content="The Pomodoro technique plus gamification" />
+                    <meta property="og:image" content="images/open_granph_body.png" />
+                    <link rel="shortcut icon" href="favicon.png" type="image/png" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
                 </Head>
